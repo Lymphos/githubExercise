b9a71b190e0cc4e81f86d3056cbbf27ade364eaf
Lymphos <taha_yildirim@sfu.ca>
2025-07-21 01:28:31 -0700

    Changed the structure of the code to make it clearer, changed some variable names, added another Boolean variable for legend.

--- circularTahasV.R
+++ circularTahasV.R
@@ -1,61 +1,61 @@
 library("circular")
-library("knitr")
-library("readxl")
-library("ggplot2")
 library("dplyr")
+
 #### Parameters to change #####
 # Data file name to read;
 circularData <- read.csv("example_hormone_acrophase_data.csv")
 # Choose the column (by index) with time data;
-columnParameter = 2
+columnParameter <- 2
 # Title of the circular plot;
 graphTitle <- "Saliva Cortisol"
 # Color for data points and arrow;
-colorGraph = "blue"
+colorGraph <- "blue"
 # Show arrow for circular mean or not (Assign TRUE or FALSE);
-circMeanArrowBool = TRUE
-
+circMeanArrowBool <- FALSE
+# Show/Create circular mean and variance legend at the bottom left of graph (Assign TRUE or FALSE):
+legendCircMeanVar <- TRUE
 ######### Hour labels on the graph #############################################
 hour_labels <- c("0", "3", "6", "9", "12", "15", "18", "21")
-hour_pos <- as.numeric(hour_labels)
+hourLabel_positions <- as.numeric(hour_labels)
 
 # Adjust angles for clock24 template (clockwise, 0 at top)
-label_angles <- (0.5 * pi - (hour_pos / 24) * 2 * pi) %% (2 * pi)
+label_angles <- (0.5 * pi - (hourLabel_positions / 24) * 2 * pi) %% (2 * pi)
 
 # Compute coordinates for labels
-x_pos <- 0.8 * cos(label_angles)
-y_pos <- 0.8 * sin(label_angles)
+x_pos_label <- 0.8 * cos(label_angles)
+y_pos_label <- 0.8 * sin(label_angles)
 
 ########## Creating the Graph ##################################################
-myParam <- circularData[columnParameter]
+paramToGraph <- circularData[columnParameter]
 
-myParam.circ <- circular(myParam, units = "hours", template = "clock24")
-myParam.circ.mean <- mean(myParam.circ, na.rm = T)
-myParam.circ.var <- var(myParam.circ, na.rm = T)
+paramToGraph.circ <- circular(paramToGraph, units = "hours", template = "clock24")
+paramToGraph.circ.mean <- mean(paramToGraph.circ, na.rm = T)
+paramToGraph.circ.var <- var(paramToGraph.circ, na.rm = T)
 
-plot.circular(myParam.circ, col = colorGraph, cex = 0.72, pch = 16,
+plot.circular(paramToGraph.circ, col = colorGraph, cex = 0.72, pch = 16,
               stack = TRUE, axes = FALSE, start.sep = 0.05, sep = 0.07,
               bins = 72, ticks = FALSE, tcl = 0.05, tol = 0.77, units = "hours",
               template = "clock24", rotation = "clock", zero = 0, lwd = 1,
               control.circle = circle.control(lwd = 2.5, cex = 2), shrink = 0.7)
 title(graphTitle, line = -0.5)
-if(circMeanArrowBool){arrows.circular(myParam.circ.mean, length = 0.1, lwd = 2, col = colorGraph, shrink = 0.65)}
+if(circMeanArrowBool){arrows.circular(paramToGraph.circ.mean, length = 0.1, lwd = 2, col = colorGraph, shrink = 0.65)}
+################# Label adding to the graph ####################################
+# Add labels to the circular plot
+text(x_pos_label, y_pos_label, labels = hour_labels, cex = 0.8)
 
 ##################### Legend and Table ###################################
-legend("bottomleft",
+if(legendCircMeanVar){legend("bottomleft",
        legend = c(
-         paste("Mean:", round(myParam.circ.mean, 2)),
-         paste("Var.:", round(myParam.circ.var, 2))
+         paste("Mean:", round(paramToGraph.circ.mean, 2)),
+         paste("Var.:", round(paramToGraph.circ.var, 2))
        ),
        text.col = colorGraph,
        bty = "n", cex = 0.8)
 
 stats_table <- data.frame(
   Metric = c("Circular Mean", "Circular Variance"),
-  Value = round(c(as.numeric(myParam.circ.mean), myParam.circ.var), 3)
+  Value = round(c(as.numeric(paramToGraph.circ.mean), paramToGraph.circ.var), 3)
 )
+}
 View(stats_table)
-################# Label adding to the graph ####################################
-# Add labels to the circular plot
-text(x_pos, y_pos, labels = hour_labels, cex = 0.8)
 
