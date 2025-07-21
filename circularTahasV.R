c4955e1fe291ad94dd917c2729ecbc9c7fa43101
Lymphos <taha_yildirim@sfu.ca>
2025-07-21 01:01:45 -0700

    Changed the names of variables and added some titles for clarity

--- circularTahasV.R
+++ circularTahasV.R
@@ -11,11 +11,11 @@ columnParameter = 2
 # Title of the circular plot;
 graphTitle <- "Saliva Cortisol"
 # Color for data points and arrow;
-colGraph = "blue"
+colorGraph = "blue"
 # Show arrow for circular mean or not (Assign TRUE or FALSE);
 circMeanArrowBool = TRUE
 
-################################################################################
+######### Hour labels on the graph #############################################
 hour_labels <- c("0", "3", "6", "9", "12", "15", "18", "21")
 hour_pos <- as.numeric(hour_labels)
 
@@ -25,20 +25,21 @@ label_angles <- (0.5 * pi - (hour_pos / 24) * 2 * pi) %% (2 * pi)
 # Compute coordinates for labels
 x_pos <- 0.8 * cos(label_angles)
 y_pos <- 0.8 * sin(label_angles)
-################################################################################
+
+########## Creating the Graph ##################################################
 myParam <- circularData[columnParameter]
 
 myParam.circ <- circular(myParam, units = "hours", template = "clock24")
 myParam.circ.mean <- mean(myParam.circ, na.rm = T)
 myParam.circ.var <- var(myParam.circ, na.rm = T)
 
-plot.circular(myParam.circ, col = colGraph, cex = 0.72, pch = 16,
+plot.circular(myParam.circ, col = colorGraph, cex = 0.72, pch = 16,
               stack = TRUE, axes = FALSE, start.sep = 0.05, sep = 0.07,
               bins = 72, ticks = FALSE, tcl = 0.05, tol = 0.77, units = "hours",
               template = "clock24", rotation = "clock", zero = 0, lwd = 1,
               control.circle = circle.control(lwd = 2.5, cex = 2), shrink = 0.7)
 title(graphTitle, line = -0.5)
-if(circMeanArrowBool){arrows.circular(myParam.circ.mean, length = 0.1, lwd = 2, col = colGraph, shrink = 0.65)}
+if(circMeanArrowBool){arrows.circular(myParam.circ.mean, length = 0.1, lwd = 2, col = colorGraph, shrink = 0.65)}
 
 ##################### Legend and Table ###################################
 legend("bottomleft",
@@ -46,7 +47,7 @@ legend("bottomleft",
          paste("Mean:", round(myParam.circ.mean, 2)),
          paste("Var.:", round(myParam.circ.var, 2))
        ),
-       text.col = colGraph,
+       text.col = colorGraph,
        bty = "n", cex = 0.8)
 
 stats_table <- data.frame(
@@ -54,7 +55,7 @@ stats_table <- data.frame(
   Value = round(c(as.numeric(myParam.circ.mean), myParam.circ.var), 3)
 )
 View(stats_table)
-################################################################################
+################# Label adding to the graph ####################################
 # Add labels to the circular plot
 text(x_pos, y_pos, labels = hour_labels, cex = 0.8)
 
